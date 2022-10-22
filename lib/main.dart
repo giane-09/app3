99ab4e97cb8c7f457ed6cf92471952435e284f2c
giane-09 <gianelaamv@gmail.com>
2022-10-22 04:18:45 +0100

    avanc

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'IMC App',
+      theme: ThemeData(
+        fontFamily: 'manrope',
+        sliderTheme: SliderThemeData(
+          activeTickMarkColor: Color(0XFFf72585).withOpacity(0.85),
+          inactiveTrackColor: Color(0XFF1d3557).withOpacity(0.85),
+          thumbColor: Color(0XFFf72585).withOpacity(0.85),
+          overlayColor: Color(0XFFf72585).withOpacity(0.85),
+          thumbShape: RoundSliderThumbShape(
+            enabledThumbRadius: 14.0,
+          ),
+          overlayShape: RoundSliderOverlayShape(
+            overlayRadius: 26.0,
+            ),
+            trackHeight: 8.0,
+        )
+       ),
       home: HomePage()
     );
   }
